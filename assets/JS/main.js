606d834f0369bc2b6a1fb48a0d07354d75d119da
Michael Fenwick <mickyfen17@aol.com>
2016-10-14 17:39:59 -0700

    Created global variables and now have temp toggle working

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,12 @@
 jQuery(document).ready(function($) {
 	//Declare golbal variables
-	var lat;
-	var lon;
+	var lat, lon, location;
+	var temp_c, temp_f, weather, weatherIcon, dailyLowF, dailyHighF, dailyLowC, dailyHighC, forecastTxt, forecastTxtMetric;
+	var plusOneIcon, plusTwoIcon, plusThreeIcon, plusOneforecastTxt, plusTwoforecastTxt, plusThreeforecastTxt;
+	var plusOneforecastTxtMetric, plusTwoforecastTxtMetric, plusThreeforecastTxtMetric;
+	var plusOneDailyHighF, plusTwoDailyHighF, plusThreeDailyHighF, plusOneDailyHighC, plusTwoDailyHighC, plusThreeDailyHighC;
+	var plusOneDailyLowF, plusTwoDailyLowF, plusThreeDailyLowF, plusOneDailyLowC, plusTwoDailyLowC, plusThreeDailyLowC;
+
 	var degC = String.fromCharCode(176) + "C";
 	var degF = String.fromCharCode(176) + "F";
 	var weatherIcons = {
@@ -110,78 +115,86 @@ jQuery(document).ready(function($) {
 		if(data.response.error) {
 			alert(data.response.error.description);
 		}
-		var location = data.current_observation.display_location.full;
-		var temp_c = data.current_observation.temp_c;
-		var temp_f = data.current_observation.temp_f;
-		var weather = data.current_observation.weather;
 		var windSpeed = data.current_observation.wind_mph;
 		var windDir = data.current_observation.wind_dir;
 		var sunriseHour = data.sun_phase.sunrise.hour;
 		var sunriseMin = data.sun_phase.sunrise.minute;
 		var sunsetHour = data.sun_phase.sunset.hour;
 		var sunsetMin = data.sun_phase.sunset.minute;
-		var dailyHighC = data.forecast.simpleforecast.forecastday[0].high.celsius; 
-		var dailyLowC = data.forecast.simpleforecast.forecastday[0].low.celsius;
-		var dailyHighF = data.forecast.simpleforecast.forecastday[0].high.fahrenheit; 
-		var dailyLowF = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
-		var weatherIcon = data.current_observation.icon;
 		var today = data.forecast.txt_forecast.forecastday[0].title;
-		var forecastTxtMetric = data.forecast.txt_forecast.forecastday[0].fcttext_metric;
-		var forecastTxt = data.forecast.txt_forecast.forecastday[0].fcttext;
+		location = data.current_observation.display_location.full;
+		temp_c = data.current_observation.temp_c;
+		temp_f = data.current_observation.temp_f;
+		weather = data.current_observation.weather;
+		dailyHighC = data.forecast.simpleforecast.forecastday[0].high.celsius; 
+		dailyLowC = data.forecast.simpleforecast.forecastday[0].low.celsius;
+		dailyHighF = data.forecast.simpleforecast.forecastday[0].high.fahrenheit; 
+		dailyLowF = data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+		weatherIcon = data.current_observation.icon;
+
+		forecastTxtMetric = data.forecast.txt_forecast.forecastday[0].fcttext_metric;
+		forecastTxt = data.forecast.txt_forecast.forecastday[0].fcttext;
+
+		$("#degC").prop("checked", true);
+		toggleTemp();
 
 		$("#currentLocation").text(location);
-		$("#currentWeather").text(weather + ", " + temp_c + degC);
-		$("#currentWeather").prepend("<span><i class='wi " + weatherIcons[weatherIcon] + "'></i></span> ");
 		$(".forecast p").html("Today " + "<span><strong>" + today + "</strong></span>");
-		$("#forecastTxt").text(forecastTxtMetric);
 		$("#currentWindSpeed").html("<i class='wi wi-strong-wind'></i><span> " + windSpeed + " mph " + windDir + "</span>");
 		$("#astronomy .sunrise").html("<i class='wi wi-sunrise'></i> 0" + sunriseHour + ":" + sunriseMin);
 		$("#astronomy .sunset").html("<i class='wi wi-sunset'></i> " + sunsetHour + ":" + sunsetMin);
+		// comment below
+		$("#currentWeather").text(weather + ", " + temp_c + degC);
+		$("#currentWeather").prepend("<span><i class='wi " + weatherIcons[weatherIcon] + "'></i></span> ");
+		$("#forecastTxt").text(forecastTxtMetric);
 		$("#highLow .high span").text(dailyHighC + degC);		
 		$("#highLow .low span").text(dailyLowC + degC);	
+		// comment above
 	}
 
 	function futureWeather(data) {
 		var plusOneDay = data.forecast.simpleforecast.forecastday[1].date.weekday_short;
 		var plusOneMonth = data.forecast.simpleforecast.forecastday[1].date.monthname_short;
 		var plusOneDate = data.forecast.simpleforecast.forecastday[1].date.day;
-		var plusOneIcon = data.forecast.simpleforecast.forecastday[1].icon;
 		var plusOneHumid = data.forecast.simpleforecast.forecastday[1].avehumidity;
-		var plusOneforecastTxtMetric = data.forecast.txt_forecast.forecastday[2].fcttext_metric;
-		var plusOneforecastTxt = data.forecast.txt_forecast.forecastday[2].fcttext;
-		var plusOneDailyHighC = data.forecast.simpleforecast.forecastday[1].high.celsius; 
-		var plusOneDailyLowC = data.forecast.simpleforecast.forecastday[1].low.celsius;
-		var plusOneDailyHighF = data.forecast.simpleforecast.forecastday[1].high.fahrenheit; 
-		var plusOneDailyLowF = data.forecast.simpleforecast.forecastday[1].low.fahrenheit;
+		plusOneIcon = data.forecast.simpleforecast.forecastday[1].icon;
+		plusOneforecastTxtMetric = data.forecast.txt_forecast.forecastday[2].fcttext_metric;
+		plusOneforecastTxt = data.forecast.txt_forecast.forecastday[2].fcttext;
+		plusOneDailyHighC = data.forecast.simpleforecast.forecastday[1].high.celsius; 
+		plusOneDailyLowC = data.forecast.simpleforecast.forecastday[1].low.celsius;
+		plusOneDailyHighF = data.forecast.simpleforecast.forecastday[1].high.fahrenheit; 
+		plusOneDailyLowF = data.forecast.simpleforecast.forecastday[1].low.fahrenheit;
 
 		var plusTwoDay = data.forecast.simpleforecast.forecastday[2].date.weekday_short;
 		var plusTwoMonth = data.forecast.simpleforecast.forecastday[2].date.monthname_short;
 		var plusTwoDate = data.forecast.simpleforecast.forecastday[2].date.day;
-		var plusTwoIcon = data.forecast.simpleforecast.forecastday[2].icon;
 		var plusTwoHumid = data.forecast.simpleforecast.forecastday[2].avehumidity;
-		var plusTwoforecastTxtMetric = data.forecast.txt_forecast.forecastday[4].fcttext_metric;
-		var plusTwoforecastTxt = data.forecast.txt_forecast.forecastday[4].fcttext;
-		var plusTwoDailyHighC = data.forecast.simpleforecast.forecastday[2].high.celsius; 
-		var plusTwoDailyLowC = data.forecast.simpleforecast.forecastday[2].low.celsius;
-		var plusTwoDailyHighF = data.forecast.simpleforecast.forecastday[2].high.fahrenheit; 
-		var plusTwoDailyLowF = data.forecast.simpleforecast.forecastday[2].low.fahrenheit;
+		plusTwoIcon = data.forecast.simpleforecast.forecastday[2].icon;
+		plusTwoforecastTxtMetric = data.forecast.txt_forecast.forecastday[4].fcttext_metric;
+		plusTwoforecastTxt = data.forecast.txt_forecast.forecastday[4].fcttext;
+		plusTwoDailyHighC = data.forecast.simpleforecast.forecastday[2].high.celsius; 
+		plusTwoDailyLowC = data.forecast.simpleforecast.forecastday[2].low.celsius;
+		plusTwoDailyHighF = data.forecast.simpleforecast.forecastday[2].high.fahrenheit; 
+		plusTwoDailyLowF = data.forecast.simpleforecast.forecastday[2].low.fahrenheit;
 
 		var plusThreeDay = data.forecast.simpleforecast.forecastday[3].date.weekday_short;
 		var plusThreeMonth = data.forecast.simpleforecast.forecastday[3].date.monthname_short;
 		var plusThreeDate = data.forecast.simpleforecast.forecastday[3].date.day;
-		var plusThreeIcon = data.forecast.simpleforecast.forecastday[3].icon;
 		var plusThreeHumid = data.forecast.simpleforecast.forecastday[3].avehumidity;
-		var plusThreeforecastTxtMetric = data.forecast.txt_forecast.forecastday[6].fcttext_metric;
-		var plusThreeforecastTxt = data.forecast.txt_forecast.forecastday[6].fcttext;
-		var plusThreeDailyHighC = data.forecast.simpleforecast.forecastday[3].high.celsius; 
-		var plusThreeDailyLowC = data.forecast.simpleforecast.forecastday[3].low.celsius;
-		var plusThreeDailyHighF = data.forecast.simpleforecast.forecastday[3].high.fahrenheit; 
-		var plusThreeDailyLowF = data.forecast.simpleforecast.forecastday[3].low.fahrenheit;
+		plusThreeIcon = data.forecast.simpleforecast.forecastday[3].icon;
+		plusThreeforecastTxtMetric = data.forecast.txt_forecast.forecastday[6].fcttext_metric;
+		plusThreeforecastTxt = data.forecast.txt_forecast.forecastday[6].fcttext;
+		plusThreeDailyHighC = data.forecast.simpleforecast.forecastday[3].high.celsius; 
+		plusThreeDailyLowC = data.forecast.simpleforecast.forecastday[3].low.celsius;
+		plusThreeDailyHighF = data.forecast.simpleforecast.forecastday[3].high.fahrenheit; 
+		plusThreeDailyLowF = data.forecast.simpleforecast.forecastday[3].low.fahrenheit;
 
+		toggleTemp();
+		
 		$("#plusOne p").text(plusOneDay + ", " + plusOneMonth + " " + plusOneDate);
 		$("#plusTwo p").text(plusTwoDay + ", " + plusTwoMonth + " " + plusTwoDate);
 		$("#plusThree p").text(plusThreeDay + ", " + plusThreeMonth + " " + plusThreeDate);
-
+		// comment below
 		$("#plusOne .plusForecast").html("<span><i class='wi " + weatherIcons[plusOneIcon] + "'></i></span> " +
 			"<div>" + plusOneforecastTxtMetric + "</div>");
 		$("#plusTwo .plusForecast").html("<span><i class='wi " + weatherIcons[plusTwoIcon] + "'></i></span> " +
@@ -195,63 +208,57 @@ jQuery(document).ready(function($) {
 		$("#plusTwo .plusHighLow .low span").text(plusTwoDailyLowC + degC);
 		$("#plusThree .plusHighLow .high span").text(plusThreeDailyHighC + degC);
 		$("#plusThree .plusHighLow .low span").text(plusThreeDailyLowC + degC);
-
+		// comment above
 		$("#plusOne .plusHighLow .humid span").text(plusOneHumid + "%");
 		$("#plusTwo .plusHighLow .humid span").text(plusTwoHumid + "%");
 		$("#plusThree .plusHighLow .humid span").text(plusThreeHumid + "%");
 	}
+
+	function toggleTemp() {
+		$("input[type='radio']").on("click", function() {
+			if($("#degF").prop("checked")) {
+				$("#currentWeather").text(weather + ", " + temp_f + degF);
+				$("#currentWeather").prepend("<span><i class='wi " + weatherIcons[weatherIcon] + "'></i></span> ");
+				$("#highLow .high span").text(dailyHighF + degF);		
+				$("#highLow .low span").text(dailyLowF + degF);	
+				$("#forecastTxt").text(forecastTxt);
+
+				$("#plusOne .plusForecast").html("<span><i class='wi " + weatherIcons[plusOneIcon] + "'></i></span> " +
+					"<div>" + plusOneforecastTxt + "</div>");
+				$("#plusTwo .plusForecast").html("<span><i class='wi " + weatherIcons[plusTwoIcon] + "'></i></span> " +
+					"<div>" + plusTwoforecastTxt + "</div>");
+				$("#plusThree .plusForecast").html("<span><i class='wi " + weatherIcons[plusThreeIcon] + "'></i></span> " +
+					"<div>" + plusThreeforecastTxt + "</div>");
+
+				$("#plusOne .plusHighLow .high span").text(plusOneDailyHighF + degF);
+				$("#plusOne .plusHighLow .low span").text(plusOneDailyLowF + degF);
+				$("#plusTwo .plusHighLow .high span").text(plusTwoDailyHighF + degF);
+				$("#plusTwo .plusHighLow .low span").text(plusTwoDailyLowF + degF);
+				$("#plusThree .plusHighLow .high span").text(plusThreeDailyHighF + degF);
+				$("#plusThree .plusHighLow .low span").text(plusThreeDailyLowF + degF);
+				// console.log("Deg F checked");
+			} else {
+				$("#currentWeather").text(weather + ", " + temp_c + degC);
+				$("#currentWeather").prepend("<span><i class='wi " + weatherIcons[weatherIcon] + "'></i></span> ");
+				$("#highLow .high span").text(dailyHighC + degC);		
+				$("#highLow .low span").text(dailyLowC + degC);	
+				$("#forecastTxt").text(forecastTxtMetric);
+
+				$("#plusOne .plusForecast").html("<span><i class='wi " + weatherIcons[plusOneIcon] + "'></i></span> " +
+					"<div>" + plusOneforecastTxtMetric + "</div>");
+				$("#plusTwo .plusForecast").html("<span><i class='wi " + weatherIcons[plusTwoIcon] + "'></i></span> " +
+					"<div>" + plusTwoforecastTxtMetric + "</div>");
+				$("#plusThree .plusForecast").html("<span><i class='wi " + weatherIcons[plusThreeIcon] + "'></i></span> " +
+					"<div>" + plusThreeforecastTxtMetric + "</div>");
+
+				$("#plusOne .plusHighLow .high span").text(plusOneDailyHighC + degC);
+				$("#plusOne .plusHighLow .low span").text(plusOneDailyLowC + degC);
+				$("#plusTwo .plusHighLow .high span").text(plusTwoDailyHighC + degC);
+				$("#plusTwo .plusHighLow .low span").text(plusTwoDailyLowC + degC);
+				$("#plusThree .plusHighLow .high span").text(plusThreeDailyHighC + degC);
+				$("#plusThree .plusHighLow .low span").text(plusThreeDailyLowC + degC);
+				// console.log("Deg C checked");
+			}
+		});
+	}
 });
-
-
-// $("input[type='radio']").on("click", function() {
-// 	if($("#degF").prop("checked")) {
-// 		$("#currentWeather").text(weather + ", " + temp_f + degF);
-// 		$("#currentWeather").prepend("<span><i class='wi " + weatherIcons[weatherIcon] + "'></i></span> ");
-// 		$("#highLow .high span").text(dailyHighF + degF);		
-// 		$("#highLow .low span").text(dailyLowF + degF);	
-// 		$("#forecastTxt").text(forecastTxt);
-// 		console.log("Deg F checked");
-// 	} else {
-// 		$("#currentWeather").text(weather + ", " + temp_c + degC);
-// 		$("#currentWeather").prepend("<span><i class='wi " + weatherIcons[weatherIcon] + "'></i></span> ");
-// 		$("#highLow .high span").text(dailyHighC + degC);		
-// 		$("#highLow .low span").text(dailyLowC + degC);	
-// 		$("#forecastTxt").text(forecastTxtMetric);
-// 		console.log("Deg C checked");
-// 	}
-// });
-
-
-// $("input[type='radio']").on("click", function() {
-// 	if($("#degF").prop("checked", true)) {
-// 		$("#plusOne .plusForecast").html("<span><i class='wi " + weatherIcons[plusOneIcon] + "'></i></span> " +
-// 			"<div>" + plusOneforecastTxt + "</div>");
-// 		$("#plusTwo .plusForecast").html("<span><i class='wi " + weatherIcons[plusTwoIcon] + "'></i></span> " +
-// 			"<div>" + plusTwoforecastTxt + "</div>");
-// 		$("#plusThree .plusForecast").html("<span><i class='wi " + weatherIcons[plusThreeIcon] + "'></i></span> " +
-// 			"<div>" + plusThreeforecastTxt + "</div>");
-
-// 		$("#plusOne .plusHighLow .high span").text(plusOneDailyHighF + degF);
-// 		$("#plusOne .plusHighLow .low span").text(plusOneDailyLowF + degF);
-// 		$("#plusTwo .plusHighLow .high span").text(plusTwoDailyHighF + degF);
-// 		$("#plusTwo .plusHighLow .low span").text(plusTwoDailyLowF + degF);
-// 		$("#plusThree .plusHighLow .high span").text(plusThreeDailyHighF + degF);
-// 		$("#plusThree .plusHighLow .low span").text(plusThreeDailyLowF + degF);
-// 		console.log("Deg F checked");
-// 	} else {
-// 		$("#plusOne .plusForecast").html("<span><i class='wi " + weatherIcons[plusOneIcon] + "'></i></span> " +
-// 			"<div>" + plusOneforecastTxtMetric + "</div>");
-// 		$("#plusTwo .plusForecast").html("<span><i class='wi " + weatherIcons[plusTwoIcon] + "'></i></span> " +
-// 			"<div>" + plusTwoforecastTxtMetric + "</div>");
-// 		$("#plusThree .plusForecast").html("<span><i class='wi " + weatherIcons[plusThreeIcon] + "'></i></span> " +
-// 			"<div>" + plusThreeforecastTxtMetric + "</div>");
-
-// 		$("#plusOne .plusHighLow .high span").text(plusOneDailyHighC + degC);
-// 		$("#plusOne .plusHighLow .low span").text(plusOneDailyLowC + degC);
-// 		$("#plusTwo .plusHighLow .high span").text(plusTwoDailyHighC + degC);
-// 		$("#plusTwo .plusHighLow .low span").text(plusTwoDailyLowC + degC);
-// 		$("#plusThree .plusHighLow .high span").text(plusThreeDailyHighC + degC);
-// 		$("#plusThree .plusHighLow .low span").text(plusThreeDailyLowC + degC);
-// 		console.log("Deg C checked");
-// 	}
-// });
